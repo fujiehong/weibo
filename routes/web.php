692106dcb84e0718e8d18f84dc3f924960ef47d4
soso <fujiehong@foxmail.com>
2019-11-22 04:02:39 +0000

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/','StaticPagesController@home');
+Route::get('/about','StaticPagesController@about');
+Route::get('/help','StaticPagesController@help');
