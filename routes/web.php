458cc9f68ed894d6288f9b9277e09fb224194960
soso <fujiehong@foxmail.com>
2019-11-22 09:04:59 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/about','StaticPagesController@about')->name('about');
 Route::get('/help','StaticPagesController@help')->name('help');
 
 Route::get('signup','UsersController@create')->name('signup');
+
+Route::resource('Users','UsersController');
+Route::get('/users/{user}','UsersController@show')->name('users.show');
