b493be064587f2765ad2e36c8eaa1d688a12429f
soso <fujiehong@foxmail.com>
2019-11-27 10:57:02 +0000

    发布微博

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Statuses;
+use Auth;
+
+class StatusesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:140'
+        ]);
+        //一对多情况下，一个用户拥有N个微博。所以发布的时候用下面方法进行创建新微博。
+        Auth::user()->statuses()->create([
+            'content' => $request['content']
+        ]);
+
+        session()->flash('success', '发布成功！');
+        return redirect()->back();
+
+    }
+}
