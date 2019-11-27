08b5f2eb06bf181455e069cdfe8a6ab38449d50d
soso <fujiehong@foxmail.com>
2019-11-27 15:21:55 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,20 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderBy('created_at','desc');
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
+
 }
